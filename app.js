28531f0937ac824e24eabc386be43c06dfad5f85
kraken9x <47894224+kraken9x@users.noreply.github.com>
2020-03-30 17:53:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// Exercise for Number/String/Boolean - For/While loop - If/else statements
+
+// 1. Cho người dùng nhập vào số bất kỳ.
+//    Kiểm tra xem số có âm hay không
+//    Nếu âm => print(invalid input)
+//    Nếu dương => Tính giai thừa của số đó và in ra
+// 2. Cho người dùng nhập vào name + age
+// Nếu tuổi < 18 => print(name chưa đủ tuổi vào trang web này)
+// Nếu tuổi >=18 => tiếp tục hỏi: name có muốn đăng nhập vào trang web này không
+//                              : nếu câu trả lời = yes => print(name đã vào trang web thành công)
+//                              : nếu câu trả lời = no  => print(name thoát khỏi trang web)
+
+// 3. Cho người dùng nhập vào username + password,
+//    Tạo một biến username2 và password2 tạm thời ở code
+//    Kiểm tra username === username2 và password === password2
+//    Nếu cả 2 đều giống => print(đăng nhập thành công)
+//    Nếu một trong 2 sai => print(username sai: nếu username sai) hoặc print(password sai: nếu pass sai)
+
+// 4. Tìm hiểu các lấy thứ, ngày, tháng và câu lệnh switch/case trong javascript (google)
+// Cho người dùng nhập vào tháng sinh(month) và name
+// Nếu sinh 1,2,3 => name sinh vào mùa xuân
+// Nếu sinh 4,5,6 => name sinh vào mùa hạ
+// Nếu sinh 7,8,9 => name sinh vào mùa thu
+// Nếu sinh 10,11,12 => name sinh vào mùa đông
+// Nhập các trường hợp khác không hợp lệ => input không hợp lệ
+
+// 5. Cho người dùng nhập vào 3 tham số a, b, c của một phương trình bậc 2=> Tìm nghiệm của phương trình đó
+// (sử dụng câu lệnh if/else)
+// Sau khi in ra nghiệm thì tiếp tục hỏi người dùng có muốn sử dụng chương trình tiếp không
+// Nếu yes => Ứng dụng vòng lặp while cho chương trình chạy tiếp
+// Nếu no  => Dừng chương trình
+
+// 6. Cách truy cập vào từng phần tử của 1 chuỗi
+// VD: const randomString = "Hello World";
+// console.log(randomString[0]) => // in ra chữ "H"
+// console.log(randomString[1]) => // in ra chữ "e"
+// console.log(randomString[2]) => // in ra chữ "l"
+// chỉ số bắt đầu của chuỗi randomString là 0 và kết thúc = randomString.length - 1
+// .length lấy ra độ dài của chuỗi đó
+
+// Ứng dụng kiến thức trên cho:
+
+// Viết 1 chương trình cho người dùng nhập vào 1 chuỗi bất kỳ
+// In ra chuỗi đó theo dạng ngược lại
+// Sau khi in ra tiếp tục hỏi người dùng có muốn tiếp tục chương trình
+// Nếu yes => dùng vòng lặp while để tiếp tục chương trình
+// Nếu no => dừng chương trình
